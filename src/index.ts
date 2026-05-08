020dfc0f2add18f8a2a66c625f63d3f1f96b9678
Gab Paderog <gabpaderog.dev@gmail.com>
2026-05-08 16:18:10 +0800

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import express from "express";
 import router from "./modules/tracking/tracking.route.js";
 import requestIp from "request-ip";
+import cors from "cors";
 
 const app = express();
 const port = "3000";
+const corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
 
 app.set("trust proxy", true);
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get("/", (req, res) => {
